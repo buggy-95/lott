596970aaafce9da89276fef4c581ffd4d9d9f167
Qín <sdoasddd0921@163.com>
2025-05-06 17:49:09 +0800

    feat: 解析复试彩票字符串

--- internal/lottery/common.go
+++ internal/lottery/common.go
@@ -136,7 +136,7 @@ func ParseLottery(input string) (Lottery, error) {
 }
 
 // 将号码区解析为前区和后区
-func ParseNumParts(input string) ([]int, []int, error) {
+func parseNumParts(input string) ([]int, []int, error) {
 	var (
 		dan []int  // 胆码
 		tuo []int  // 拖码
@@ -152,11 +152,11 @@ func ParseNumParts(input string) ([]int, []int, error) {
 			}
 
 			nextNumberType = "tuo"
-
-			return nil
+		} else {
+			return fmt.Errorf("号码区解析错误，不支持的状态流转: %s。输入: %s", next, input)
 		}
 
-		return fmt.Errorf("号码区解析错误, 不支持的状态流转: %s。输入: %s", next, input)
+		return nil
 	}
 
 	dealNumber := func() error {
@@ -268,7 +268,7 @@ func ParseNumParts(input string) ([]int, []int, error) {
 			continue
 		}
 
-		return nil, nil, fmt.Errorf("号码区解析失败, 错误的字符: 【%c】。输入: %s", char, input)
+		return nil, nil, fmt.Errorf("号码区解析失败，错误的字符: 【%c】。输入: %s", char, input)
 	}
 
 	if err := dealNumber(); err != nil {
@@ -284,7 +284,197 @@ func ParseNumParts(input string) ([]int, []int, error) {
 
 // DLT:01,02,03,04~05,06-07~08x3:25053
 func ParseComplexLotteryParts(input string) (ComplexLotteryParts, error) {
+	nextTokenType := "type" // type -> front -> back -> scale | index -> index | scale
 	complexLotteryParts := ComplexLotteryParts{}
+	complexLotteryParts.Scale = 1
+
+	var (
+		token       string
+		scaleParsed bool
+		indexParsed bool
+	)
+
+	switchNextTokenType := func(next string) error {
+		commonErrorMsg := fmt.Errorf("解析失败。错误的token类型: %s, 当前token类型: %s。输入: %s", next, nextTokenType, input)
+
+		switch next {
+		case "front":
+			if nextTokenType != "type" {
+				return commonErrorMsg
+			}
+		case "back":
+			if nextTokenType != "front" {
+				return commonErrorMsg
+			}
+		case "scale":
+			if scaleParsed {
+				return fmt.Errorf("解析失败。倍投已解析过。输入: %s", input)
+			}
+
+			if !(nextTokenType == "back" || nextTokenType == "index") {
+				return commonErrorMsg
+			}
+		case "index":
+			if indexParsed {
+				return fmt.Errorf("解析失败。期号已解析过。输入: %s", input)
+			}
+
+			if !(nextTokenType == "back" || nextTokenType == "scale") {
+				return commonErrorMsg
+			}
+		}
+
+		nextTokenType = next
+
+		return nil
+	}
+
+	dealToken := func(tokenType string) error {
+		tmpToken := token
+		token = ""
+
+		switch tokenType {
+		case "type":
+			if tmpToken == "SSQ" || tmpToken == "DLT" {
+				complexLotteryParts.Type = tmpToken
+			} else {
+				return fmt.Errorf("彩票类型解析失败。不支持的彩票类型: %s。输入: %s", tmpToken, input)
+			}
+		case "front":
+			dan, tuo, err := parseNumParts(tmpToken)
+
+			if err != nil {
+				return fmt.Errorf("前区号码解析失败。原因: %s。输入: %s", err, input)
+			} else {
+				complexLotteryParts.FrontDan = dan
+				complexLotteryParts.FrontTuo = tuo
+			}
+		case "back":
+			dan, tuo, err := parseNumParts(tmpToken)
+
+			if err != nil {
+				return fmt.Errorf("后区号码解析失败。原因: %s。输入: %s", err, input)
+			} else {
+				complexLotteryParts.BackDan = dan
+				complexLotteryParts.BackTuo = tuo
+			}
+		case "scale":
+			scale, err := strconv.Atoi(tmpToken)
+
+			if err != nil {
+				return fmt.Errorf("倍投倍数解析失败。倍数: %s。输入: %s", tmpToken, input)
+			} else {
+				complexLotteryParts.Scale = scale
+				scaleParsed = true
+			}
+		case "index":
+			index, err := strconv.Atoi(tmpToken)
+
+			if err != nil {
+				return fmt.Errorf("期号解析失败。期号: %s。输入: %s", tmpToken, input)
+			} else {
+				complexLotteryParts.Index = index
+				indexParsed = true
+			}
+		}
+
+		return nil
+	}
+
+	dealChar := func(char rune) error {
+		switch nextTokenType {
+		case "type":
+			if char == ':' {
+				if err := dealToken("type"); err != nil {
+					return err
+				}
+
+				if err := switchNextTokenType("front"); err != nil {
+					return err
+				}
+			} else if len(token) < 5 && ('A' <= char && char <= 'Z') {
+				token += string(char)
+			} else {
+				return fmt.Errorf("彩票类型解析失败。输入: %s", input)
+			}
+		case "front":
+			if char == '-' {
+				if err := dealToken("front"); err != nil {
+					return err
+				}
+
+				if err := switchNextTokenType("back"); err != nil {
+					return err
+				}
+			} else if char == ',' || char == '~' || ('0' <= char && char <= '9') {
+				token += string(char)
+			} else {
+				return fmt.Errorf("前区号码解析失败。当前字符: 【%c】。输入: %s", char, input)
+			}
+		case "back":
+			if char == 'x' {
+				if err := dealToken("back"); err != nil {
+					return err
+				}
+
+				if err := switchNextTokenType("scale"); err != nil {
+					return err
+				}
+			} else if char == ':' {
+				if err := dealToken("back"); err != nil {
+					return err
+				}
+
+				if err := switchNextTokenType("index"); err != nil {
+					return err
+				}
+			} else if char == ',' || char == '~' || ('0' <= char && char <= '9') {
+				token += string(char)
+			} else {
+				return fmt.Errorf("后区号码解析失败。输入: %s", input)
+			}
+		case "scale":
+			if char == ':' {
+				if err := dealToken("scale"); err != nil {
+					return err
+				}
+
+				if err := switchNextTokenType("index"); err != nil {
+					return err
+				}
+			} else if '0' <= char && char <= '9' {
+				token += string(char)
+			} else {
+				return fmt.Errorf("倍投数解析失败。当前字符: 【%c】。输入: %s", char, input)
+			}
+		case "index":
+			if char == 'x' {
+				if err := dealToken("index"); err != nil {
+					return err
+				}
+
+				if err := switchNextTokenType("scale"); err != nil {
+					return err
+				}
+			} else if '0' <= char && char <= '9' {
+				token += string(char)
+			} else {
+				return fmt.Errorf("期数解析失败。当前字符: 【%c】。输入: %s", char, input)
+			}
+		}
+
+		return nil
+	}
+
+	for _, char := range input {
+		if err := dealChar(char); err != nil {
+			return ComplexLotteryParts{}, err
+		}
+	}
+
+	if err := dealToken(nextTokenType); err != nil {
+		return ComplexLotteryParts{}, err
+	}
 
 	return complexLotteryParts, nil
 }
